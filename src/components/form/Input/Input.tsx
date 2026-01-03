b9579315a80a7e7c27f7403b42fa9024d3c92c9a
TGauquelin <timothegauquelin@gmail.com>
2026-01-03 15:55:46 +0100

    created profile page & updated Input to be able to make it disabled and required

--- src/components/form/Input/Input.tsx
+++ src/components/form/Input/Input.tsx
@@ -4,19 +4,26 @@ interface InputProps {
     type: 'text' | 'email' | 'password';
     value?: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    required?: boolean;
+    disabled?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({ label, placeholder, type, value, onChange }) => {
+const Input: React.FC<InputProps> = ({ label, placeholder, type, value, onChange, required = false, disabled = false }) => {
     return (
-        <div className="flex flex-col gap-2">
-            <label htmlFor={label.toLowerCase()} className="text-base font-semibold">{label}</label>
+        <div className="flex flex-col gap-2 flex-1">
+            <label htmlFor={label.toLowerCase()} className="text-base font-semibold">
+                {label}
+                {required && <span className="text-red-500 ml-1">*</span>}
+            </label>
             <input 
                 type={type} 
                 id={label.toLowerCase()} 
-                className="w-full p-3 rounded-md border border-gray-outline focus:outline-none focus:border-2 focus:border-true-blue" 
+                className="w-full p-3 rounded-md border border-gray-outline focus:outline-none focus:border-2 focus:border-true-blue disabled:bg-gray-100 disabled:cursor-not-allowed" 
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
+                disabled={disabled}
+                required={required}
             />
         </div>
     );
--- src/pages/private/profilePage/ProfilePage.tsx
+++ src/pages/private/profilePage/ProfilePage.tsx
@@ -1,7 +1,98 @@
+import Input from "@/components/form/Input/Input";
+import { useAuth } from "@/stores/authStore";
+import { UserWithPassword } from "@/types/auth.types";
+import { useState, useEffect } from "react";
+
 const ProfilePage: React.FC = () => {
+
+    const { user } = useAuth();
+
+    const [userData, setUserData] = useState<UserWithPassword | null>(null);
+
+    const handleInputChange = (key: keyof UserWithPassword) => (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        if (userData) {
+            setUserData({
+                ...userData,
+                [key]: e.target.value
+            });
+        }
+    };
+
+    useEffect(() => {
+        if (user) {
+            setUserData(user as UserWithPassword);
+        }
+    }, [user]);
+
+    if (!user) {
+        return (
+            <div className="flex flex-col gap-4">
+                <h2 className="text-2xl font-bold">Profile Page</h2>
+                <div className="text-red-600">Please log in to view your profile</div>
+            </div>
+        );
+    }
+
+    if (!userData) {
+        return (
+            <div className="flex flex-col gap-4">
+                <h2 className="text-2xl font-bold">Profile Page</h2>
+                <div>Loading...</div>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            Profile Page
+        <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold">Profile Page</h2>
+            <div className="flex flex-col gap-2">
+                <label htmlFor="avatar" className="text-base font-bold">Picture</label>
+                <div className="w-[80px] h-[80px] overflow-hidden">
+                    <img 
+                        src={user.avatar} 
+                        alt="Avatar" 
+                        id="avatar"
+                        className="w-full h-full rounded-full object-cover border border-4 border-true-blue" 
+                    />
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <Input
+                    label="Firstname"
+                    type="text"
+                    placeholder="Jane"
+                    value={userData.firstName}
+                    onChange={handleInputChange('firstName')}
+                    required={true}
+                    disabled={true}
+                />
+                <Input
+                    label="Lastname"
+                    type="text"
+                    placeholder="Doe"
+                    value={userData.lastName}
+                    onChange={handleInputChange('lastName')}
+                    required={true}
+                    disabled={true}
+                />
+            </div>
+            <div className="flex gap-4">
+                <Input
+                    label="Email"
+                    type="email"
+                    placeholder="jane.doe@gmail.com"
+                    value={userData.email}
+                    onChange={handleInputChange('email')}
+                    required={true}
+                    disabled={true}
+                />
+            </div>
+            <div className="flex flex-col gap-2">
+                <span className="text-base font-bold">Role</span>
+                <span className="w-fit text-base font-bold text-true-blue bg-light-blue rounded-md px-2 py-1">{user.role}</span>
+            </div>
         </div>
     );
 };
